4547b14c61ff0f2fd4c5707f21b70c6fa602d466
KFS Developer <dev@kreatekflow.com>
2026-06-18 17:25:53 -0400

    Fix typescript error in pushNotifications for Vercel build

--- src/lib/pushNotifications.ts
+++ src/lib/pushNotifications.ts
@@ -43,8 +43,8 @@ export const triggerLocalPush = async (title: string, body: string, icon?: strin
       body,
       icon: icon || '/favicon.ico',
       badge: '/favicon.ico',
-      vibrate: [200, 100, 200],
+      vibrate: [200, 100, 200] as any,
       requireInteraction: true
-    });
+    } as any);
   }
 };
